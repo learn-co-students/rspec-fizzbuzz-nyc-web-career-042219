191b8b58c9116252153d03362eae34ac11b04cf3
Carly Pecora <carly@carlypecora.com>
2019-04-05 11:10:48 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-	if num % 3 == 0
-		"Fizz"
+	if num % 3 == 0 && num % 5 == 0
+		"FizzBuzz"
 	elsif num % 5 == 0
 		"Buzz"
-	elsif num % 3 == 0 && num % 5 == 0
-		"Fizzbuzz"
+	elsif num % 3 == 0
+		"Fizz"
 	else
-		puts nil
+		nil
+	end
 end
